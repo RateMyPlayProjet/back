34ad33c9f6de3d72904440da2173f7ecb584558a
ines <bourhim.ines@gmail.com>
2024-03-31 15:41:09 +0200

    createNoticeFromUserId : OK

--- src/Controller/NoticeController.php
+++ src/Controller/NoticeController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use App\Entity\User;
+use App\Entity\Notice;
 use App\Repository\NoticeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class NoticeController extends AbstractController
 {
@@ -35,5 +44,57 @@ class NoticeController extends AbstractController
         return new JsonResponse($jsonGame,200,[],true);
     }
 
+    #[Route('/api/notice/{idNotice}', name: 'notice.get', methods: ['GET'])]
+    #[ParamConverter("notice", options: ["id" => "idNotice"])]
     
+    public function getNotice(Notice $notice, SerializerInterface $serializer): JsonResponse{
+        $jsonNotice= $serializer->serialize($notice,'json', ['groups'=> "getAllNotices"]);
+        return new JsonResponse($jsonNotice,200,[],true);
+    }
+
+
+    #[Route('/api/user/{userId}/notices/new', name:'create_notice_for_user', methods: ['POST'])]
+    public function createNoticeForUser(Request $request,SerializerInterface $serializer,TagAwareCacheInterface $cache,EntityManagerInterface $entityManager,UrlGeneratorInterface $urlGenerator,ValidatorInterface $validator,int $userId): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+        
+        $userRepository = $entityManager->getRepository(User::class);
+
+        // Récupérez l'utilisateur à partir de l'ID.
+        $user = $userRepository->find($userId);
+        if (!$user) {
+            return new JsonResponse(['error' => 'User not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $notice = $serializer->deserialize($request->getContent(), Notice::class, 'json');
+        $dateNow = new \DateTime();
+        
+        $notice
+            ->setUser($user)
+            ->setCreateAt($dateNow)
+            ->setUpdateAt($dateNow);
+
+        // Ecrire game s'il est donné
+        if (isset($data['gameId'])) {
+            $game = $entityManager->getRepository(Game::class)->find($data['gameId']);
+            if ($game) {
+                $notice->setGame($game);
+            } else {
+                return new JsonResponse(['error' => 'Game not found'], JsonResponse::HTTP_NOT_FOUND);
+            }
+        }
+
+        $errors = $validator->validate($notice);
+        if (count($errors) > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+        
+        $entityManager->persist($notice);
+        $entityManager->flush();
+        
+        $jsonNotice = $serializer->serialize($notice, 'json');
+        
+        $location = $urlGenerator->generate('notice.get', ['id' => $notice->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        
+        return new JsonResponse($jsonNotice, Response::HTTP_CREATED, ["Location" => $location], true);
+    }    
 }
